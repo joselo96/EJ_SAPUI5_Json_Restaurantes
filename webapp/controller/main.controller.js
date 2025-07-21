458f44338b04fac1dcfb9748715148001843cfd2
Jose Venegas <jose-luis-9898@hotmail.com>
2025-07-21 16:59:42 +0000

    Ejercicio mas completo se adiciona columba y se deja funcionando

--- webapp/controller/main.controller.js
+++ webapp/controller/main.controller.js
@@ -4,11 +4,13 @@ sap.ui.define([
     "sap/ui/model/FilterOperator",
     "sap/ui/model/Filter",
     "sap/ui/core/Fragment",
-    "sap/m/MessageBox"
-], (Controller, MessageToast, FilterOperator, Filter, Fragment, MessageBox) => {
+    "sap/m/MessageBox",
+    "inetum/restaurantes/model/formatter"
+], (Controller, MessageToast, FilterOperator, Filter, Fragment, MessageBox,formatter) => {
     "use strict";
 
     return Controller.extend("inetum.restaurantes.controller.main", {
+        formatter:formatter,
         onInit() {
         },
         onFilterName: function (oEvent) {
@@ -37,7 +39,7 @@ sap.ui.define([
             oBinding.filter(aFilters, "Application");
         },
 
-        OnOpenFragment: function (oEvent) {
+        OnOpenFragment: function () {
             if (!this.oAddDialog) {
                 Fragment.load({
                     id: this.createId("IdAddRestaurant"),
@@ -54,6 +56,7 @@ sap.ui.define([
 
 
             let oInputNombre = this.byId("IdAddRestaurant--IdFgNombre")
+            let oInputTipo = this.byId("IdAddRestaurant--IdFgTipo")
             let oInputUbicacion = this.byId("IdAddRestaurant--IdFgUbicacion")
             let oinputRanking = this.byId("IdAddRestaurant--RatingInd")
             let oInputPlatos = this.byId("IdAddRestaurant--IdFgPlatos")
@@ -63,7 +66,8 @@ sap.ui.define([
 
 
 
-            let aControls = [oInputNombre,
+            let aControlsAdd = [oInputNombre,
+                oInputTipo,
                 oInputUbicacion,
                 // oinputRanking,
                 oInputPlatos,
@@ -71,7 +75,7 @@ sap.ui.define([
                 oInputTelefono,
                 oInputSitioWeb]
 
-            aControls.forEach((oControl) => {
+            aControlsAdd.forEach((oControl) => {
                 oControl.setValueState("None");
             });
 
@@ -89,13 +93,13 @@ sap.ui.define([
               this._oDialog.open();
               */
         },
-        OnOpenFragmentUpdate: function (oEvent) {
+        OnOpenFragmentUpdate: function () {
 
             let oTable = this.byId("IdRestaurante")
             let oSelectedIndex = oTable.getSelectedIndex()
 
             if (oSelectedIndex === -1) {
-                MessageToast.show("Por favor Seleccione una fila")
+                MessageToast.show("Por favor Seleccione un registro")
                 return
             }
 
@@ -107,8 +111,8 @@ sap.ui.define([
             oModel.setProperty("/nuevoRestaurante", {
                 Nombre: oRaw.Nombre,
                 Ubicación: oRaw.Ubicación,
-                //Estrellas: oRaw[2].Estrellas,
-                //Estrellas: oRaw.Estrellas,
+                Tipo_de_comida : oRaw.Tipo_de_comida,
+                Estrellas: oRaw.Estrellas,
                 Cantidad_de_platos: oRaw.Cantidad_de_platos,
                 Rango_de_precios: oRaw.Rango_de_precios,
                 Teléfono: oRaw.Teléfono,
@@ -117,6 +121,7 @@ sap.ui.define([
 
 
             let oInputNombre = this.byId("IdUpdateRestaurant--_IDGenInp1ut")
+            let oInputTipo = this.byId("IdUpdateRestaurant--IdFgTipoUp")
             let oInputUbicacion = this.byId("IdUpdateRestaurant--_IDG1enInput1")
             //let oInputRanking = this.byId("IdUpdateRestaurant--Ratin1gInd")
             let oInputPlatos = this.byId("IdUpdateRestaurant--_IDG1enInput2")
@@ -125,6 +130,7 @@ sap.ui.define([
             let oInputSitioWeb = this.byId("IdUpdateRestaurant--_IDGenfI1nput1")
 
             let aControls = [oInputNombre,
+                oInputTipo,
                 oInputUbicacion,
                 //oinputRanking,
                 oInputPlatos,
@@ -157,16 +163,16 @@ sap.ui.define([
 
 
         },
-        OnValidateDatos: function (aControls, oInputRanking) {
+        OnValidateDatos: function (aControlsValidate,oInputRankingValidate) {
 
             let bValid = true // Booleano que dira si todos las validaciones son correctas 
 
 
-            aControls.forEach((oControl) => {
+            aControlsValidate.forEach((oControl) => {
                 oControl.setValueState("None");
             });
 
-            aControls.forEach((oControl) => {
+            aControlsValidate.forEach((oControl) => {
                 if (oControl.getValue() == "" || oControl.getValue() == null) {
                     oControl.setValueState("Error")
                     oControl.setValueStateText("El campo es obligatorio")
@@ -174,27 +180,27 @@ sap.ui.define([
                 }
             })
 
-            if (oInputRanking.getValue() == 0) {
+            if (oInputRankingValidate.getValue() == 0) {
                 bValid = false;
                 MessageBox.warning("Por favor indique Numero de Estrellas");
             }
 
-            if (aControls[2].getValue() > 50) {
+            if (aControlsValidate[3].getValue() > 50) {
 
-                aControls[2].setValueState("Error")
-                aControls[2].setValueStateText("No se permiten mas de 50 platos")
+                aControlsValidate[3].setValueState("Error")
+                aControlsValidate[3].setValueStateText("No se permiten mas de 50 platos")
                 bValid = false;
             }
 
-            if (!(aControls[3].getValue() == "Alto" || aControls[3].getValue() == "Medio" || aControls[3].getValue() == "Bajo")) {
-                aControls[3].setValueState("Error")
-                aControls[3].setValueStateText("Debe seleccionar una de las opciones")
+            if (!(aControlsValidate[4].getValue() == "Alto" || aControlsValidate[4].getValue() == "Medio" || aControlsValidate[4].getValue() == "Bajo")) {
+                aControlsValidate[4].setValueState("Error")
+                aControlsValidate[4].setValueStateText("Debe seleccionar una de las opciones")
                 bValid = false;
             }
 
-            if (aControls[4].getValue().length > 10 || aControls[4].getValue().length < 10) {
-                aControls[4].setValueState("Error")
-                aControls[4].setValueStateText("Ingrese un numero de telefono valido")
+            if (aControlsValidate[5].getValue().length > 10 || aControlsValidate[5].getValue().length < 10) {
+                aControlsValidate[5].setValueState("Error")
+                aControlsValidate[5].setValueStateText("Ingrese un numero de telefono valido")
                 bValid = false;
             }
 
@@ -205,6 +211,7 @@ sap.ui.define([
         OnAddRestaurant: function () {
 
             let oInputNombre = this.byId("IdAddRestaurant--IdFgNombre")
+            let oInputTipo = this.byId("IdAddRestaurant--IdFgTipo")
             let oInputUbicacion = this.byId("IdAddRestaurant--IdFgUbicacion")
             let oinputRanking = this.byId("IdAddRestaurant--RatingInd")
             let oInputPlatos = this.byId("IdAddRestaurant--IdFgPlatos")
@@ -214,7 +221,8 @@ sap.ui.define([
 
 
 
-            let aControls = [oInputNombre,
+            let aControlsAdd = [oInputNombre,
+                oInputTipo,
                 oInputUbicacion,
                 //oinputRanking,
                 oInputPlatos,
@@ -222,13 +230,14 @@ sap.ui.define([
                 oInputTelefono,
                 oInputSitioWeb]
 
-            if (this.OnValidateDatos(aControls, oinputRanking, oInputPrecios,)) {
+            if (this.OnValidateDatos(aControlsAdd, oinputRanking)) {
 
                 let oModel = this.getOwnerComponent().getModel("mModeloDatos");
                 let oNewRestaurant = oModel.getProperty("/nuevoRestaurante");
                 let aRestaurant = oModel.getProperty("/Restaurantes");
                 const oAdd = {
                     Nombre: oNewRestaurant.Nombre,
+                    Tipo_de_comida: oNewRestaurant.Tipo_de_comida,
                     Ubicación: oNewRestaurant.Ubicación,
                     Estrellas: oNewRestaurant.Estrellas,
                     Cantidad_de_platos: oNewRestaurant.Cantidad_de_platos,
@@ -242,16 +251,17 @@ sap.ui.define([
                 MessageToast.show("Restaurante creado con exito")
                 oModel.setProperty("/Restaurantes", aRestaurant)
 
-                onCloseDialog();
+                this.onCloseDialog();
 
 
 
             }
 
         },
-        OnUpdateRestaurant: function (oEvent) {
+        OnUpdateRestaurant: function () {
 
             let oInputNombreUp = this.byId("IdUpdateRestaurant--_IDGenInp1ut")
+            let oInputTipo = this.byId("IdUpdateRestaurant--IdFgTipoUp")
             let oInputUbicacionUp = this.byId("IdUpdateRestaurant--_IDG1enInput1")
             let oInputRankingUp = this.byId("IdUpdateRestaurant--Ratin1gInd")
             let oInputPlatosUp = this.byId("IdUpdateRestaurant--_IDG1enInput2")
@@ -262,6 +272,7 @@ sap.ui.define([
             //let bValid = true // Booleano que dira si todos las validaciones son correctas 
 
             let aControlsUp = [oInputNombreUp,
+                oInputTipo,
                 oInputUbicacionUp,
                 //oInputRankingUp,
                 oInputPlatosUp,
@@ -280,12 +291,13 @@ sap.ui.define([
 
                 const aUpdate = {
                     Nombre: aControlsUp[0].getValue(),
-                    Ubicación: aControlsUp[1].getValue(),
+                    Tipo_de_comida: aControlsUp[1].getValue(),
+                    Ubicación: aControlsUp[2].getValue(),
                     Estrellas: oInputRankingUp.getValue(),
-                    Cantidad_de_platos: aControlsUp[2].getValue(),
-                    Rango_de_precios: aControlsUp[3].getValue(),
-                    Teléfono: aControlsUp[4].getValue(),
-                    Sitio_web: aControlsUp[5].getValue()
+                    Cantidad_de_platos: aControlsUp[3].getValue(),
+                    Rango_de_precios: aControlsUp[4].getValue(),
+                    Teléfono: aControlsUp[5].getValue(),
+                    Sitio_web: aControlsUp[6].getValue()
                 }
                 aRestaurant[oSelectedIndex] = aUpdate;
                 this.onCloseDialog()
@@ -301,10 +313,10 @@ sap.ui.define([
 
             oModel.setProperty("/nuevoRestaurante", {
                 Nombre: "",
+                Tipo_de_comida: "",
                 Ubicación: "",
                 Estrellas: 0,
                 Cantidad_de_platos: null,
-                Tipo_de_comida: "",
                 Rango_de_precios: "",
                 Teléfono: null,
                 Sitio_web: ""
@@ -347,8 +359,7 @@ sap.ui.define([
 
 
         },
-      
-
+        
 
 
 
